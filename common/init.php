48a2afbcfe6453afc7139fe56b630d92db324c0c
wzbwzb_187@126.com <hahazhi@gmail.com>
2013-07-24 22:13:21 +0800

    init

---
+++ common/init.php
@@ -0,0 +1,22 @@
+<?php
+include ROOTDIR."/config/config.php";
+
+include ROOTDIR."/class/weibo_sdk/saetv2.ex.class.php";
+include ROOTDIR."/class/mysql.class.php";
+include ROOTDIR."/class/generator.class.php";
+include ROOTDIR."/common/function.php";
+
+
+$db =  mysql::getHandle($db_config);
+
+if(!$db) {
+	echo "db not connection!";
+	exit;
+}
+$params = parseUrl($rewrite_config);
+
+if(!empty($params))
+	foreach($params as $k => $param)
+{
+	$_GET[$k] = $param;
+}
