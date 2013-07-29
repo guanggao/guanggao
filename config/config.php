7884e0c6b06a9a248ed91c805a5e5aa678907166
wzbwzb_187@126.com <hahazhi@gmail.com>
2013-07-30 00:07:18 +0800

    config

--- config/config.php
+++ config/config.php
@@ -3,28 +3,7 @@ $db_config = array(
 		'host'=>'127.0.0.1',
 		'user'=>'root',
 		'pass'=>'',
-		'dbname'=>'union',
+		'dbname'=>'guanggao',
 		'charset'=>'utf8'
 );
 
-
-$rewrite_config = array(
-		//首页
-		'/'=>array('app'=>'front','action'=>'index'),
-
-		//管理后台
-		'/admin/?'=>array('app'=>'admin','action'=>'index'),
-		'/admin/header/?'=>array('app'=>'admin','action'=>'header'),
-		'/admin/menu/?'=>array('app'=>'admin','action'=>'menu'),
-		'/admin/main/?'=>array('app'=>'admin','action'=>'main'),
-		'/admin/user/?'=>array('app'=>'admin','action'=>'user'),
-		'/admin/user_modify/?'=>array('app'=>'admin','action'=>'user_modify'),
-		'/admin/_modify_user/?'=>array('app'=>'admin','action'=>'_modify_user'),
-
-		//网站主管理
-		
-);
-
-
-//不需要登陆action
-$nologin_action = array('login', 'reg', 'code');
